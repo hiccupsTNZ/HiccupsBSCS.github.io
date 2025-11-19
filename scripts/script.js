bae685ca1bc408f752d57f29ec846ed409c7317f
hiccupsTNZ <hyikejt@gmail.com>
2025-11-19 22:05:13 +0800

    Add rice meals menu and update HTML generation

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,7 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-  new Splide( '#card-carousel', {
-		perPage    : 2,
-		breakpoints: {
-			640: {
-				perPage: 1,
-			},
-		},
-  } ).mount();
-} );
-
 const menuRamen = ["Tantanmen Ramen", "Tonkotsu Ramen",  "Chashu Ramen","Shoyu Ramen", "Curry Ramen", "Seafood Ramen"];
 const priceRamen = ["410","410","420","400","410","420"];
+
+const menuRicemeals = ["Omurice", "Chashudon", "Gyudon Original" , "Gyudon Spicy", "Gyudon Cheese","Chahan","Tonkatsu"]
+const priceRicemeals = ["350","300","320","330","340","300","400"];
 document.getElementById("ramen-list").innerHTML = menuRamen.map((item, i) => `<tr><td>${item}</td><td>${priceRamen[i]}</td></tr>`).join("");
+document.getElementById("ricemeals-list").innerHTML = menuRicemeals.map((item, i) => `<tr><td>${item}</td><td>${priceRicemeals[i]}</td></tr>`).join("");
